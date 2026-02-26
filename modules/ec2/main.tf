75207b9f2503df0ad62551d1c95db65ac63f2bb1
Vasile Bejan <124290984+vasile007@users.noreply.github.com>
2026-02-26 00:28:20 +0000

    Add remote Terraform backend (S3 + DynamoDB)

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,35 @@
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "${var.name}-ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.name}-ec2-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+
+
+
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -9,6 +41,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
@@ -17,13 +50,20 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install nginx -y
-              systemctl enable nginx
-              systemctl start nginx
-              echo "<h1>ConfessionVerse is Live 🚀</h1>" > /var/www/html/index.html
-              EOF
+#!/bin/bash
+apt update -y
+apt install -y nginx snapd
+
+snap install amazon-ssm-agent --classic
+systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
+systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service
+
+systemctl enable nginx
+systemctl start nginx
+echo "<h1>ConfessionVerse is Live 🚀</h1>" > /var/www/html/index.html
+EOF
+
+
 
   tags = merge(var.tags, {
     Name = "${var.name}-ec2-web"
